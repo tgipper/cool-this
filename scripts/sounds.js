84f46476fea0aa28d82b1f663a6dfad815db1144
Trevor Gipper <trevor.gipper@gmail.com>
2017-05-17 21:52:51 -0600

    Add music examples and tempo zones

---
+++ scripts/sounds.js
@@ -0,0 +1,17 @@
+//click a player button to begin playing
+//play the click sound file once through
+//play the drum sound file
+//loop the drum sound file
+//end the playback on click of player button
+//when clicked again, begin process from playing the click sound
+
+var audioPlayer = function() {
+    
+}
+
+document.getElementById("hero").addEventListener("click", addBorder);
+
+
+var addBorder = function(event) {
+    console.log("test");
+}

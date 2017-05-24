e09f2a841bc1c4a4d012dd73ffc763c6f2b8f872
Trevor Gipper <trevor.gipper@gmail.com>
2017-05-24 15:43:07 -0700

    first set of exercises are playable, click and loop not sequenced

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -1,23 +1,38 @@
-//click a player button to begin playing
-//play the click sound file once through
-//play the drum sound file
-//loop the drum sound file
-//end the playback on click of player button
-//when clicked again, begin process from playing the click sound
+var playButtons = document.getElementsByClassName("audio");
 
-var audioPlayer = function() {
-    
+for ( var i = 0; i < playButtons.length; i++) {
+	playButtons[i].addEventListener("click", playTrack);
 }
 
-var table = document.getElementById("exercise-table");
+var currentlyPlaying = false;
 
-var addBorder = function() {
-    console.log("test");
-    
-    table.style.border = 4px solid red;
+function playTrack() {
+	var clickPath = "../assets/audio/clicks/";
+	var clickName = this.dataset.clickName;
+	var trackPath = "../assets/audio/drums/";
+	var trackName = this.dataset.trackName;
+	
+	clickPath = clickPath + clickName;
+	
+	trackPath = trackPath + trackName;
+	
+	var click = new Audio(clickPath);
+	var track = new Audio(trackPath);
+
+	click.play();
+
+	if (click.played.end == click.duration) {
+		console.log(click.duration);
+	}
+	//check click duration
+	//check click current time
+	//if click currentTime == duation, play track
+	//check track duration
+	//check track current time
+	//if track currentTime == duration, play track
+	track.play();
+	console.log("test")
 }
 
-
-document.getElementById("hero").addEventListener("click", addBorder);
-
-
+// select all of the play buttons
+// loop through them and attach an event listener to each one
